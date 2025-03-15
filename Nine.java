a893dc7634cd55ef95bad7c502f0fa6023dc2a69
arshita <arshitapatel077@gmail.com>
2025-03-15 15:21:38 +0530

    CONDITIONAL STATEMENTS QUESTIONS Ternary Operator

---
+++ Nine.java
@@ -0,0 +1,10 @@
+//Question 4 : What will be the value of x & y in the following
+import java.util.*;
+public class Nine {
+    public static void main(String args[]) {
+        int a = 63, b = 36;
+        boolean x = (a < b ) ? true : false;
+        int y= (a > b ) ? a : b;
+        System.out.println("value of x "+x+"& Y =" +y);
+    }
+}
