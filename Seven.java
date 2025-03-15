f8ccf1c1cef81d9e26e5ba92fa5bf0acef228bee
arshita <arshitapatel077@gmail.com>
2025-03-15 14:48:56 +0530

    CONDITIONAL STATEMENTS QUESTIONS if-else

---
+++ Seven.java
@@ -0,0 +1,11 @@
+public class Seven {
+    public static void main(String args[]){
+
+        double temp=103.5;
+        if(temp>100){
+            System.out.println("I have a fever");
+        }else {
+            System.out.println("you don't have fever");
+        }
+    }
+}
