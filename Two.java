5f40d2c57268e6879ed67893c72fe9599eb5aef3
arshita <arshitapatel077@gmail.com>
2025-03-15 13:37:54 +0530

    OPERATORS QUESTIONS

---
+++ Two.java
@@ -0,0 +1,14 @@
+public class Two {
+    public static void main(String arhs[]){
+        int x=200;
+        int y=50;
+        int z=100;
+        if(x>y && y>z){
+            System.out.println("HELLO");
+        }if(z>y && z<x){
+            System.out.println("JAVA");
+        }if((y+200)<x && (y+150)<z){
+            System.out.println("Hello Java");
+        }
+    }
+}
