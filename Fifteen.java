cbb58eb6f2320f8bb5db278eaf593af3b4594746
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-03-16 10:49:29 +0530

    updated condition

--- Fifteen.java
+++ Fifteen.java
@@ -3,16 +3,20 @@ public class Fifteen {
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         int k=sc.nextInt();
-        int sum = 0;
+        int oddsum = 0;
+        int evensum=0;
         for (int i = 1; i <= k; i++) {
             int n = sc.nextInt();
             if (n % 2 == 0) {
                 System.out.println("that is even number:" + n);
+                evensum=evensum+n;
+                System.out.println("that is even number sum:" +evensum);
             } else {
                 System.out.println("that is odd number:" + n);
+                oddsum=oddsum+n;
+                System.out.println("that is even number sum:" +oddsum);
             }
-            sum=sum+n;
-            System.out.println("sum of the even and odd: "+ sum);
+
         }
 
     }
