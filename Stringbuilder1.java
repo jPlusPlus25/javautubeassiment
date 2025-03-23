0534202edb613b7e6015130ea6d8f1dc60a07d7d
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-03-23 16:45:53 +0530

    added file Stringbuilder1.java

---
+++ Stringbuilder1.java
@@ -0,0 +1,9 @@
+public class Stringbuilder1 {
+    public static void main(String args[]){
+        StringBuilder sb=new StringBuilder("");
+        for(char ch='a';ch<='z';ch++){
+            sb.append(ch);
+            System.out.println(sb);
+        }System.out.println(sb);
+    }
+}
