3a339827204e9838f38e78175126b830f8db5a25
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-03-26 23:35:25 +0530

    file update

--- Recurtion8.java
+++ Recurtion8.java
@@ -6,7 +6,18 @@ public class Recurtion8 {
         int xn=x*xnm1;
         return xn;
     }
+    //optimized power
+    public static int optimizedPow(int a,int n){
+        if(n==1){
+            return a;
+        }
+        int halfpowerSq=optimizedPow(a,n/2)*optimizedPow(a,n/2);
+        //n is odd
+        if(n%2!=0){
+            halfpowerSq=a*halfpowerSq;
+        }return halfpowerSq;
+    }
     public static void main(String args[]){
-        System.out.println(power(2,10));
+        System.out.println(power(2,11));
     }
 }
