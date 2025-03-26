2362ee57b1f3aef5d112dcebda5f98816dae68b6
arshitapatel077@gmail.com <arshitapatel077@gmail.com>
2025-03-26 23:12:25 +0530

    added file Recurtion8.java

---
+++ Recurtion8.java
@@ -0,0 +1,12 @@
+public class Recurtion8 {
+    public static int power(int x,int n){
+        if(n==1){
+            return x;
+        }int xnm1=power(x,n-1);
+        int xn=x*xnm1;
+        return xn;
+    }
+    public static void main(String args[]){
+        System.out.println(power(2,10));
+    }
+}
